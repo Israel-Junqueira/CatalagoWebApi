b432063bf17b19aa34f45440c061bc283729aa0a
Israel Ribeiro Junqueira <israelribeiro313@gmail.com>
2023-08-30 16:45:26 -0300

    modelo-Data-connectionString created

---
+++ ApiCatalogo/ApiCatalogo/Data/AppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using ApiCatalogo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCatalogo.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet <Categoria> categorias { get; set; }
+        public DbSet<Produto> produto { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           
+        }
+    }
+}
---
+++ ApiCatalogo/Dominio/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCatalogo.Models
+{
+    public class Categoria
+    {
+        public int? CategoriaId { get; set; }
+        public string? Nome { get; set; }
+        public string?  ImagemUrl { get; set; }
+
+        public IEnumerable<Produto>? produtos { get; set; }
+    }
+}
